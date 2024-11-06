886e9780711794a4d44611eb1f8e09eeddebe9c3
eletConnect <164717570+eletConnect@users.noreply.github.com>
2024-11-06 16:01:55 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: process.env.VITE_HOST || '0.0.0.0',
-    port: parseInt(process.env.VITE_PORT) || 5173,
+    port: process.env.VITE_PORT || 5173,
   },
   
   plugins: [
@@ -13,24 +14,44 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: false,
+
       manifest: {
         name: 'eletconnect',
         short_name: 'eletconnect',
         description: 'eletconnect',
         theme_color: '#ffffff',
+
         icons: [
-          { src: 'pwa-64x64.png', sizes: '64x64', type: 'image/png' },
-          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
-          { src: 'maskable-icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
         ],
       },
+
       workbox: {
         globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
         cleanupOutdatedCaches: true,
         clientsClaim: true,
-        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // Limite para 3 MB
       },
+
       devOptions: {
         enabled: false,
         navigateFallback: 'index.html',
@@ -39,22 +60,4 @@ export default defineConfig({
       },
     }),
   ],
-
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'react';
-            if (id.includes('luxon')) return 'luxon';
-            if (id.includes('exceljs')) return 'exceljs';
-            if (id.includes('pdf-lib')) return 'pdf-lib';
-            if (id.includes('axios')) return 'axios';
-            return 'vendor';
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1500, // Ajuste para 1500 KB para evitar o aviso
-  },
 });
